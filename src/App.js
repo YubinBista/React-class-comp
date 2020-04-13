2de77b8f725fcf4cd12d5ccd5ee49f9613d63e52
raj bista <lomaslove132@gmail.com>
2020-04-13 19:31:57 +1000

    created components

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err)=>console.log(err)
-  );
-  return (
-    <div >
-      hi
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    
+    this.state = {lat: null};
 
-    </div>
-  );
-}
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+      this.setState({
+        lat: position.coords.latitude });
+      },
+        err =>console.log(err)
+    );
+  }
+  render(){
+    return(
+    <div> latitude: {this.state.lat} </div>
+    )
+  };
+};
+  
 
 export default App;
